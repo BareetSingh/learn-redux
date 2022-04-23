709c3d667ce5dd2aca95385da8d6737c01b1574f
Bareet Singh <singhbareet@gmail.com>
2022-04-23 23:53:10 +0530

    basic understanding

--- src/index.js
+++ src/index.js
@@ -3,6 +3,51 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+
+// STORE-> Globalized State
+
+// Action-> (increment/decrement)
+const increment=()=>{
+  return{
+    type:'INCREMENT'
+  }
+}
+
+const decrement=()=>{
+  return{
+    type:'DECREMENT'
+  }
+}
+
+// Reducer
+const counter=(state=0, action)=>{
+  switch(action.type){
+    case 'INCREMENT':
+      return state+1;
+    case 'DECREMENT':
+      return state-1;
+  }
+};
+
+let store= createStore(counter);
+
+//Display it in the console
+store.subscribe(()=> console.log(store.getState()));
+
+// DISPATCH
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(decrement());
+store.dispatch(increment());
+
+
+
+
+
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
