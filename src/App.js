2c44dbadbdb627a48fc92d7dff9c94111758e252
Bareet Singh <singhbareet@gmail.com>
2022-04-24 12:04:58 +0530

    redux implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './actions';
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Learn redux</h1>
+        <h1>Counter: {counter}</h1>
+        <button onClick={() => dispatch(increment(6))}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+        {isLogged ? <h3>The Private informatio(only logged is user can see this).</h3> : ''}
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,56 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore} from 'redux';
+import { createStore } from 'redux';
+import allReducers from './reducers/index';
+import { Provider } from 'react-redux';
 
-// STORE-> Globalized State
-
-// Action-> (increment/decrement)
-const increment=()=>{
-  return{
-    type:'INCREMENT'
-  }
-}
-
-const decrement=()=>{
-  return{
-    type:'DECREMENT'
-  }
-}
-
-// Reducer
-const counter=(state=0, action)=>{
-  switch(action.type){
-    case 'INCREMENT':
-      return state+1;
-    case 'DECREMENT':
-      return state-1;
-  }
-};
-
-let store= createStore(counter);
-
-//Display it in the console
-store.subscribe(()=> console.log(store.getState()));
-
-// DISPATCH
-store.dispatch(increment());
-store.dispatch(decrement());
-store.dispatch(decrement());
-store.dispatch(increment());
-
-
-
-
-
-
-
-
+const store = createStore(allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
